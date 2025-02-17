1c67d281cc81b8383c6554f8587e269379072fb3
drujwhh <mitrusinpeta@gmail.com>
2025-02-17 11:21:59 +0300

    Add files via upload

---
+++ лаба1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int countbits(int n) {
+    int count = 0;
+    while (n > 0) {
+        count += (n & 1);
+        n >>= 1;
+    }
+    return count;
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int num;
+    std::cout << "Введите десятичное число: ";
+    std::cin >> num;
+
+    int bits = countbits(num);
+    std::cout << "Количество единиц в двоичном представлении: " << bits << std::endl;
+    return 0;
+}
